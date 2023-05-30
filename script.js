653c18644d99150a3d991838d99f8d4d106f0078
nobugsatcoding <nobugsatcoding@gmail.com>
2023-05-30 21:04:20 +0300

    added JS video play/pause, progress bar

--- script.js
+++ script.js
@@ -28,15 +28,57 @@ function togglePlay() {
 }
 
 // On video end show play button icon
-video.addEventListener('ended', showPlayIcon)
+video.addEventListener('ended', showPlayIcon);
 
 // Progress Bar ---------------------------------- //
 
+// Calc display time
+function displayTime(time) {
+    const minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+    seconds = seconds > 9 ? seconds : `0${seconds}`;
+    return `${minutes}:${seconds}`;
+}
 
+// Update progress bar as video plays
+function updateProgress() {
+    progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+    currentTime.textContent = `${displayTime(video.currentTime)} /`;
+    duration.textContent = `${displayTime(video.duration)}`;
+}
+
+// Click to seek within the video
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+    console.log(newTime);
+}
 
 // Volume Controls --------------------------- //
 
-
+// Volume Bar
+function changeVolume(e) {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    // Rounding volume
+    if(volume < 0.1) {
+        volume = 0
+    }
+    if(volume > 0.9) {
+        volume = 1
+    }
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+    // Change icon depending on volume
+    volumeIcon.className = '';
+    if(volume > 0.7) {
+        volumeIcon.classList.add('fas', 'fa-volume-up');
+    } else if(volume < 0.7 && volume > 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-down');
+    } else if(volume === 0) {
+        volumeIcon.classList.add('fas', 'fa-volume-off');
+    }
+}
 
 // Change Playback Speed -------------------- //
 
@@ -47,4 +89,8 @@ video.addEventListener('ended', showPlayIcon)
 // Event Listeners
 playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume)
 
